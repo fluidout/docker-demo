66e32242781c048b613341460e3d6b4f763354e7
supremeoverlord <fluid0028@gmail.com>
2021-02-08 02:56:37 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 echo "Deploy SolrSearchService to EC2"
 DOCKER_IMAGE=022539168230.dkr.ecr.us-east-1.amazonaws.com/solrsearch
+DOCKER_APPNAME=solrsearchservice
 docker pull $DOCKER_IMAGE
-docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
-docker run -d -p 3000:3000 $DOCKER_IMAGE
+docker container stop $DOCKER_APPNAME
+docker container rm $DOCKER_APPNAME
+docker run -d -p 3000:3000 --name $DOCKER_APPNAME $DOCKER_IMAGE
